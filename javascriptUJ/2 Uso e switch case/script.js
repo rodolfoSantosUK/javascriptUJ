9c0c4da67f996ee9a6986158df8ddca4cff13479
rodolfoSantosUK <santoseuro2018@gmail.com>
2019-07-20 16:38:33 -0300

    Switch, falsy thruthy Matriz

---
+++ javascriptUJ/2 Uso e switch case/script.js
@@ -0,0 +1,14 @@
+
+
+
+var job = 'teacher';
+
+switch(job) {
+
+case 'teacher' : console.log('Ele é um professor');
+break;
+
+case 'driver' : console.log('Ele é um motorista');
+break;
+
+}
---
+++ javascriptUJ/3 Falsy Thruthy/script.js
@@ -0,0 +1,13 @@
+
+
+
+var height;
+
+height = 0;
+// valores falsy = undefined, null, 0. ' ', NaN 
+
+if(height) {
+ console.log('Valor não está definido');
+} else {
+ console.log('Valor está definido');
+}
