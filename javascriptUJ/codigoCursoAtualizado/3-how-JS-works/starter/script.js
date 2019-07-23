3b055dbc18d9a02a7e38a2d61acc5da998d97e9e
Rodolfo Santos <rodolfo.gouveia2002@gmail.com>
2019-07-23 18:50:58 -0300

    Add files via upload

---
+++ javascriptUJ/codigoCursoAtualizado/3-how-JS-works/starter/script.js
@@ -0,0 +1,100 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+
+calculaIdade(1987);
+
+function calculaIdade(year) {
+   console.log(2016 -  year);
+}
+
+//posentadoria(1990);
+
+var aposentadoria = function(ano) {
+  console.log(65 - (2016 - ano)) ;
+}
+ 
+
+
+console.log(idade); 
+var idade =  23;
+
+function foo() {
+    var idade = 65;
+    console.log(idade);  
+}
+
+foo();
+console.log(idade);  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
+
+
+
+
+
+
+
+
+
